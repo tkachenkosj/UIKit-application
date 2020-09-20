51e662c5dfe06e92386a3747866d2999093e9f1f
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-09-20 14:14:22 +0300

    Initial Commit

---
+++ UIKit application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit application
+//
+//  Created by Сергей Ткаченко on 20.09.2020.
+//  Copyright © 2020 Sergey Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
