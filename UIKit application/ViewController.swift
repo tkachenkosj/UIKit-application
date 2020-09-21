77ec02c091b93775657b21114340d53aebf66e7f
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-09-21 13:54:29 +0300

    1. Launch screen 2. Storyboard: Segment control, Label, Slider, Text field, Button for text field 3. Viewcontroller - setting the storyboard elements by parameter

--- UIKit application/ViewController.swift
+++ UIKit application/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentSwitcher: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var sliderControl: UISlider!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var setButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Setup Label
+        mainLabel.text = ""
+        mainLabel.textAlignment = .center
+        mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.numberOfLines = 2
+        
+        // Setup Segment Control
+        segmentSwitcher.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        // Setup Slider
+        sliderControl.minimumValue = 0
+        sliderControl.maximumValue = 1
+        sliderControl.value = 0.5
+        sliderControl.maximumTrackTintColor = .red
+        
+    
     }
 
-
+    @IBAction func changeSegment() {
+        switch segmentSwitcher.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .green
+        case 2:
+            mainLabel.text = "The third segment is selected"
+            mainLabel.textColor = .blue
+        default:
+            break
+        }
+    }
+    
+    @IBAction func moveTheSlider() {
+        let backgroundColor = view.backgroundColor
+        view.backgroundColor = backgroundColor?.withAlphaComponent(CGFloat(sliderControl.value))
+        mainLabel.textColor = .white
+        mainLabel.text = String(sliderControl.value)
+    }
 }
 
